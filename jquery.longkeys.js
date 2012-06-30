c4eb8ef9aab91452243b1253c8d132c96040ab91
Evan Purcer <evanpurcer@gmail.com>
2012-06-30 15:47:21 -0700

    Implement event triggering and additional callbacks
    Plugin works properly when jquery.hotkeys is present; still requires
    testing for use without it.
    
    Triggers the custom event when the specified key is held for longer
    than "duration"; if the key is released before that, the "onShortPress"
    callback is executed. The "before" callback is executed before the
    timeout is started.

--- jquery.longkeys.js
+++ jquery.longkeys.js
@@ -1,3 +1,69 @@
 (function ($) {
 
+    $.each(['keydown', 'keypress'], function (i, type) {
+
+        var customEvent = 'long' + type;
+
+        $.event.special[customEvent] = {
+            setup: function (data) {
+
+                var el = $(this)
+                  , keyPressed   = false
+                  , timerRunning = false
+                  , keyCode = null
+
+                    // True if using jQuery Hotkeys
+                  , jQueryHotkeys = Boolean($.hotkeys && data.key);
+
+                // Bind the events: (keydown|keypress) and keyup.
+                // If the event is for a specific key,
+                // use jquery.hotkeys syntax for event binding.
+                el[type].apply(el, jQueryHotkeys ? [data.key, down] : [down]);
+                el.keyup.apply(el, jQueryHotkeys ? [data.key,  up ] : [ up ]);
+
+                // Store the handlers for later unbinding
+                el.data('handlers', { down: down, up: up });
+
+                function down(e) {
+                    if (keyPressed === false) {
+
+                        // Execute pre-delay callback
+                        if (data.before) data.before();
+
+                        // If not using jQuery Hotkeys, cache the key code
+                        // to make sure down and up handlers use the same key
+                        if (!jQueryHotkeys) keyCode = e.which;
+
+                        keyPressed = timerRunning = true;
+
+                        setTimeout(function () {
+                            timerRunning = false;
+                            if (keyPressed) el.trigger(customEvent);
+                        }, data.duration);
+                    }
+
+                    return false;
+                }
+
+                function up(e) {
+                    if (jQueryHotkeys || (e.which === keyCode)) {
+                        if (keyPressed && timerRunning) data.onShortPress();
+                        keyPressed = false;
+                        keyCode = null;
+                    }
+
+                    return false;
+                }
+            },
+
+            teardown: function () {
+                var handlers = el.data('handlers');
+                if (handlers) {
+                    if (handlers.down) $(this).unbind(type,  handlers.down);
+                    if (handlers.up)   $(this).unbind(keyup, handlers.up);
+                }
+            }
+        }
+    });
+
 })(jQuery);
